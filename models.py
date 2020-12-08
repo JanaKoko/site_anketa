3fa4811039996f4df5dc0ba3384d339093ff59b9
YanaKoko <55631770+YanaKoko@users.noreply.github.com>
2020-12-08 21:27:40 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+# In[7]:
+
+
+db = SQLAlchemy()
+
+
+# In[8]:
+
+
+class Questions(db.Model):
+
+    __tablename__ = "my_questions"
+    type_id = db.Column('qu_id', db.Integer, primary_key=True)
+    name = db.Column('qu_text', db.Text) # текстовое поле
+
+
+# In[9]:
+
+
+class People(db.Model):
+
+    __tablename__ = "my_people"
+    person_id = db.Column('pers_id', db.Integer, primary_key=True)
+    gender = db.Column('gender', db.Text)
+    age = db.Column('age', db.Text)
+    english = db.Column('eng', db.Text)
+
+
+# In[5]:
+
+
+class Answers(db.Model):
+
+    __tablename__ = "my_answers"
+    pers_id = db.Column('pers_id', db.Integer, primary_key=True)
+    one = db.Column('one', db.Integer)
+    two = db.Column('two', db.Text)
+    three = db.Column('three', db.Text)
+
+
+# In[ ]:
+
+
+
